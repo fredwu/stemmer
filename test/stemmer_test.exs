4f84c845894d5c18900604b3e8a63b3597700b4d
Fred Wu <ifredwu@gmail.com>
2016-07-19 22:27:30 +1000

    Added official `diffs.txt` for testing

--- test/stemmer_test.exs
+++ test/stemmer_test.exs
@@ -2,4 +2,23 @@ defmodule StemmerTest do
   use ExUnit.Case, async: true
 
   doctest Stemmer
+
+  test "official diffs.txt" do
+    file_path = Path.join(File.cwd!, "test/samples/diffs.tar.gz")
+    temp_path = Path.join(File.cwd!, "test/temp")
+
+    System.cmd("tar", ["xzvf", file_path, "-C", temp_path], [stderr_to_stdout: true])
+
+    Path.join(temp_path, "diffs.txt")
+    |> File.stream!()
+    |> Enum.each(fn (line) ->
+      [word, official_stemmed] = String.split(line)
+
+      # according to the official Porter2 algorithm `fluently` should be stemmed
+      # to `fluent`, however the test file suggests it stemmed to `fluentli`
+      unless word == "fluently" do
+        assert Stemmer.stem(word) == official_stemmed
+      end
+    end)
+  end
 end
