d312326f72a3d8b9ac8b154eac3683e7a8af90f3
Fred Wu <ifredwu@gmail.com>
2016-07-28 14:05:33 +1000

    Added `mix publish` with git release tagging

--- mix.exs
+++ mix.exs
@@ -2,15 +2,18 @@ defmodule Stemmer.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :stemmer,
-     version: "1.0.1",
-     elixir: "~> 1.3",
-     name: "Stemmer",
-     package: package(),
-     description: "An English (Porter2) stemming implementation in Elixir.",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps()]
+    [
+      app:             :stemmer,
+      version:         "1.0.1",
+      elixir:          "~> 1.3",
+      name:            "Stemmer",
+      package:         package(),
+      description:     "An English (Porter2) stemming implementation in Elixir.",
+      build_embedded:  Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      deps:            deps(),
+      aliases:         ["publish": ["hex.publish", "hex.docs", &git_tag/1]]
+    ]
   end
 
   def application do
@@ -27,5 +30,9 @@ defmodule Stemmer.Mixfile do
       licenses:    ["MIT"],
       links:       %{"GitHub" => "https://github.com/fredwu/stemmer"}
     ]
+  end
+
+  defp git_tag(_args) do
+    System.cmd "git", ["tag", "v" <> Mix.Project.config[:version]]
   end
 end
